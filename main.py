bc18d45caaa72d83252ac790dda222d06dbd0d76
Thiago Machado <th.machado@g.globo>
2022-07-09 16:35:23 -0300

    WIP: configurando db e modelos

--- main.py
+++ main.py
@@ -6,15 +6,21 @@ from flask_socketio import SocketIO
 from flask_bootstrap import Bootstrap
 import os
 
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 app.config['MQTT_BROKER_URL'] = os.getenv('BROKER_URL')
 app.config['MQTT_BROKER_PORT'] = int(os.getenv('BROKER_PORT'))
 app.config['MQTT_USERNAME'] = os.getenv('BROKER_USERNAME')
 app.config['MQTT_PASSWORD'] = os.getenv('BROKER_PASSWORD')
 app.config['MQTT_REFRESH_TIME'] = 1.0  # refresh time in seconds
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 BROKER_TOPIC = 'HUDDLE_MATERIAIS'
+
 mqtt = Mqtt(app)
 socketio = SocketIO(app)
+db = SQLAlchemy(app)
 
 
 @app.route("/")
@@ -42,4 +48,3 @@ if __name__ == '__main__':
     mqtt.subscribe(BROKER_TOPIC)
     print(mqtt.topics)
     app.run(host="0.0.0.0", port=5000)
-
--- models.py
+++ models.py
@@ -1,7 +1,9 @@
-from app import db
+from main import db
 
-class MaterialMeasurement(dbo.Model):
-  temperature = db.Column(db.Numeric, nullable=True)
-  humidity = db.Column(db.Numeric, nullable=True)
-  sensor_id = db.Column(db.BigInteger, nullable=True)
-  time_of_measurement = db.Column(db.DateTime, nullable=True)
+
+class MaterialMeasurement(db.Model):
+    id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
+    temperature = db.Column(db.Numeric, nullable=True)
+    humidity = db.Column(db.Numeric, nullable=True)
+    sensor_id = db.Column(db.BigInteger, nullable=True)
+    time_of_measurement = db.Column(db.DateTime, nullable=True)
