54a09789412ea9897ee993bc4aebc1e4d46eeef5
Jéssica Martins <jessica.moliveira@g.globo>
2022-07-30 20:40:55 -0300

    add equipments model

---
+++ models/equipment.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from dataclasses import dataclass
+
+from models.main import db
+
+
+@dataclass
+class EquipmentMeasurement(db.Model):
+    id: int
+    rfid: int
+    status: str
+    name: str
+    deck: str
+    timeOfMeasurement: datetime
+
+    id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
+    rfid = db.Column(db.Integer, nullable=True)
+    status = db.Column(db.String, nullable=True)
+    name = db.Column(db.String, nullable=True)
+    deck = db.Column(db.String, nullable=True)
+    timeOfMeasurement = db.Column(db.DateTime, nullable=True)
--- models/material.py
+++ models/material.py
@@ -1,7 +1,8 @@
 from datetime import datetime
-from flask_sqlalchemy import SQLAlchemy
 from dataclasses import dataclass
-db = SQLAlchemy()
+
+from models.main import db
+
 
 @dataclass
 class MaterialMeasurement(db.Model):
@@ -15,25 +16,3 @@ class MaterialMeasurement(db.Model):
     humidity = db.Column(db.Numeric, nullable=True)
     #sensorId = db.relationship('SensorsMaterial', backref='material_measurement', lazy=True)
     timeOfMeasurements = db.Column(db.DateTime, nullable=True)
-    
-
-
-
-#@dataclass
-#class SensorsMaterial:
-#    id: int
-#
-#    id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
-#    deviceId = db.relationship('Device', backref='sensors_material', lazy=True)
-#
-#    
-#
-#@dataclass
-#class Device:
-#    id: int
-#    place: str
-#    mac = str
-#
-#    id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
-#    place = db.Column(db.String,nullable=True)
-#    mac = db.Column(db.String(17),nullable=True)
