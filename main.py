4053c93651346671f96a3f345f9dec79b8f8a8bf
Jéssica Martins <jessica.moliveira@g.globo>
2022-07-30 21:08:44 -0300

    merge branch refactop-app

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
+from flask import Flask,jsonify
 import json
 from flask import jsonify
 from flask_socketio import SocketIO
 from flask_mqtt import Mqtt
 from app import setup_flask_app
-from models.models import *
+
+from models.equipment import *
+from models.material import *
 
 app = setup_flask_app()
-BROKER_TOPIC = 'HUDDLE_MATERIAIS'
+BROKER_TOPIC_MATERIAL = 'HUDDLE_MATERIAIS'
+BROKER_TOPIC_EQUIPMENT = 'HUDDLE_EQUIPAMENTOS'
 
 mqtt = Mqtt(app)
 socketio = SocketIO(app)
-result = []
-
+result_material = []
+result_equipment = []
 
 @app.route("/")
 def hello_world():
-    return {"data": result}
+    return {"data": result_material}
 
 
 @app.route("/iot/materials")
@@ -25,13 +29,13 @@ def materials():
 
 
 @mqtt.on_message()
-def handle_mqtt_message(client, userdata, message):
+def handle_mqtt_material(client, userdata, message):
     data = dict(
         topic=message.topic,
         payload=json.loads(message.payload.decode())
     )
     print(data)
-    result.append(data['payload'])
+    result_material.append(data['payload'])
     obj = data['payload']
     print(obj['temperatura'])
     with app.app_context():
@@ -43,6 +47,19 @@ def handle_mqtt_message(client, userdata, message):
     socketio.emit('mqtt_message', data=data)
 
 
+@mqtt.on_message()
+def handle_mqtt_equipment(client, userdata, message):
+    data = dict(
+        topic=message.topic,
+        payload=json.loads(message.payload.decode())
+    )
+    print(data)
+    result_equipment.append(data['payload'])
+    obj = data['payload']
+    print(obj)
+    socketio.emit('mqtt_message', data=data)
+
+
 @mqtt.on_log()
 def handle_logging(client, userdata, level, buf):
     print(level, buf)
@@ -50,6 +67,7 @@ def handle_logging(client, userdata, level, buf):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    mqtt.subscribe(BROKER_TOPIC)
+    mqtt.subscribe(BROKER_TOPIC_MATERIAL)
+    mqtt.subscribe(BROKER_TOPIC_EQUIPMENT)
     print(mqtt.topics)
     app.run(host="0.0.0.0", port=5000, debug=True)
