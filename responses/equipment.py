95629bb901a5e64bb8c2c727bd325358a99cfafb
Ian Freitas <ianfreitas1@poli.ufrj.br>
2022-08-04 00:59:34 -0300

    add equipment response schema

---
+++ responses/equipment.py
@@ -0,0 +1,9 @@
+from marshmallow import Schema, fields
+
+class EquipmentSchema(Schema):
+    id = fields.Int()
+    rfid = fields.Int()
+    status = fields.Str()
+    timeOfMeasurements = fields.DateTime()
+    name = fields.Str()
+    deck = fields.Str()
